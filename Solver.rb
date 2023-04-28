0fb04c6b7777455e31fc8bdd7c32b9b2fbed57b3
Marc raphael <marcraphael979@gmail.com>
2023-04-28 10:15:28 +0100

    Write the factorial method

--- Solver.rb
+++ Solver.rb
@@ -2,6 +2,22 @@ class Solver
 	# we have 3 methods
 	# 1: factorial
 	def factorial(int)
+		# when int is negative
+		raise RuntimeError if int.negative?
+
+		# we will need some initial values
+		# while initial is less than int
+		# the value of initial will be increased at every step in the loop
+		# factorial will be multiplied by initial at the same time
+		initial = 1
+		factorial = 1
+
+		while initial <= int
+			factorial *= initial
+			initial ++
+		end
+		
+		factorial
 	end
 	
 	# 2: fizzbuzz
