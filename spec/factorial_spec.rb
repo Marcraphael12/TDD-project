7a0f2e810b556d960b92fb0fc008acfcaf3838a0
Marc raphael <marcraphael979@gmail.com>
2023-04-28 09:26:27 +0100

    Write second test for factorial: factorial of -2 should raise a RunTime error

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -10,4 +10,8 @@ describe Solver do
   it 'makes sure the factorial of 0 is 1' do
     expect(@solver.factorial(0)).to eql 1
   end
+
+  it 'raise a runtime error when the given Integer is negative' do
+    expect{ @solver.factorial(-2) }.to raise_error(RuntimeError)
+  end
 end
