564e68dfa07fbec35bb084b6abc510b1547d3883
Marc raphael <marcraphael979@gmail.com>
2023-04-28 09:40:52 +0100

    rerurn buzz when N is divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,8 @@ describe Solver do
 	it 'retunr "fizz" when N is divisible by 3' do
 		expect(@solver.fizzbuzz(3)).to eql '"fizz"'
 	end
+
+	it 'return "buzz" whe N is divisible by 5' do
+		expect(@solver.fizzbuzz(5)).to eql '"buzz"'
+	end
 end
