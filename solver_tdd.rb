bfe219edc1bec3f4540c5f0bc3df4415bf117606
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-28 12:26:59 +0100

    Removed unused code and comments

--- solver_tdd.rb
+++ solver_tdd.rb
@@ -1,14 +1,7 @@
 class Solver
-  # we have 3 methods
-  # 1: factorial
+  
   def factorial(int)
-    # when int is negative
     raise RuntimeError if int.negative?
-
-    # we will need some initial values
-    # while initial is less than int
-    # the value of initial will be increased at every step in the loop
-    # factorial will be multiplied by initial at the same time
     initial = 1
     factorial = 1
 
@@ -31,9 +24,8 @@ class Solver
     end
   end
 
-  # 3: reverse string
+ 
   def reverse(str)
-    # just reverse the string 😅
     str.reverse
   end
 end
