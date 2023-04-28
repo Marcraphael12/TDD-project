51b2511774c4b17ddb8d99be2c7bdde6e43accf7
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-28 12:25:18 +0100

    Modify solver_tdd.rb file

--- solver_tdd.rb
+++ solver_tdd.rb
@@ -19,12 +19,7 @@ class Solver
     factorial
   end
 
-  # 2: fizzbuzz
   def fizzbuzz(int)
-    # if int % 3 and int % 5: return fizzbuzz
-    # if int % 3: return fizz
-    # if int % 5: return buzz
-    # if not: return a string
     if (int % 3).zero? && (int % 5).zero?
       '"fizzbuzz"'
     elsif (int % 3).zero?
